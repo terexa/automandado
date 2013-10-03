8abcfe22a3efbb31a085f26a4113a370d209736c
terexa.ohiggins@gmail.com <terexa.ohiggins@gmail.com>
2013-10-03 22:39:39 +0100

    changed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Add the default admin user
-@user = User.new(:email => 'admin@example.com', :password => 'admin', :password_confirmation => 'admin')
-
+# @user = User.new(:email => 'admin@example.com', :password => 'admin', :password_confirmation => 'admin')
+@user = User.new(:email => 'terexa.ohiggins@gmail.com', :password => 'qwerty00', :password_confirmation => 'qwerty00')
 @user.roles_mask = 1
 @user.save
