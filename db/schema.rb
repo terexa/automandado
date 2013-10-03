c04b5f8582880c1b0e46b01bcbf666b5770be9a3
terexa.ohiggins@gmail.com <terexa.ohiggins@gmail.com>
2013-10-03 22:31:15 +0100

    added seed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130715214345) do
+ActiveRecord::Schema.define(version: 20131003212544) do
 
   create_table "admin_menus", force: true do |t|
     t.string   "name"
