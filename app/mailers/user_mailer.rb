cd7d34b9fe78c49f69f266a3184de352e01ecdb2
terexa.ohiggins@gmail.com <terexa.ohiggins@gmail.com>
2013-10-09 21:14:03 +0100

    temporally down

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ActionMailer::Base
   # default from: "something@example.com"
-  default :from => ENV['MAIL_USERNAME']+'@'+ENV['MAIL_DOMAIN']
+  # default :from => ENV['MAIL_USERNAME']+'@'+ENV['MAIL_DOMAIN']
 
   def send_order(params)
     @user = params[:user]
