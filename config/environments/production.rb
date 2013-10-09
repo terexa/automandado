ead0eb4e21e757ba4dfa70b9691b28af957c62bf
terexa.ohiggins@gmail.com <terexa.ohiggins@gmail.com>
2013-10-09 22:22:19 +0100

    settign up production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,5 @@ Automandado::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  ActionMailer::Base.smtp_settings[:enable_starttls_auto] = false
 end
