268ff6e517f5e6392f60a23c7a7392d71cc6bd71
terexa.ohiggins@gmail.com <terexa.ohiggins@gmail.com>
2013-10-10 11:58:10 +0100

    adding ssl set up

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,11 +1,12 @@
 ActionMailer::Base.smtp_settings = {
-    :address=>              ENV['SMTP_SERVER'],
-    :port=>                 ENV['SMTP_PORT'],
-    :domain=>               ENV['MAIL_DOMAIN'],
-    :user_name=>            ENV['MAIL_USERNAME'],
-    :password=>             ENV['MAIL_PASSWORD'],
-    :authentication=>       ENV['MAIL_AUTENTICATION'],
-    :enable_starttls_auto=> true
+    :address				=> ENV['SMTP_SERVER'],
+    :port 					=> ENV['SMTP_PORT'],
+    :domain 				=> ENV['MAIL_DOMAIN'],
+    :user_name 				=> ENV['MAIL_USERNAME']+'@'+ENV['MAIL_DOMAIN'],
+    :password 				=> ENV['MAIL_PASSWORD'],
+    :authentication			=> ENV['MAIL_AUTENTICATION'],
+    :enable_starttls_auto	=> true,
+    :openssl_verify_mode	=> 'none'
 }
 
 ActionMailer::Base.default_url_options[:host] = ENV['HOST']
