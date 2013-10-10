eb1ce74105c33222377885942122adb3e4c36cf3
terexa.ohiggins@gmail.com <terexa.ohiggins@gmail.com>
2013-10-10 10:40:35 +0100

    adding string

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ActionMailer::Base
   # default from: "something@example.com"
-  default :from => ENV['MAIL_USERNAME']+'@'+ENV['MAIL_DOMAIN']
+  default :from => ENV['MAIL_USERNAME'].to_s+'@'+ENV['MAIL_DOMAIN'].to_s
 
   def send_order(params)
     @user = params[:user]
