2804175c12eeb41fa531239a78a83896a19d3128
terexa.ohiggins@gmail.com <terexa.ohiggins@gmail.com>
2013-10-11 12:29:10 +0100

    css to mail template

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
 	end
 	def send_order
 		@order = []
-		params[:menues].map{|m| @order.push( Admin::Menu.find_by_id(m).name.to_s )}
+		params[:menues].map{|m| @order.push( Admin::Menu.find_by_id(m) )}
 		order_details = {:order => @order, :user=>current_user}
 		UserMailer.send_order(order_details).deliver
 		redirect_to root_path
